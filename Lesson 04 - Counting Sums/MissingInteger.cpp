af63a6ef1df9047a189961b3b43ba0e19a0481c7
Mark Hary <44713017+markhary@users.noreply.github.com>
2018-11-28 19:55:56 -0800

    Added MissingInteger solution

--- Lesson 04 - Counting Sums/MissingInteger.cpp
+++ Lesson 04 - Counting Sums/MissingInteger.cpp
@@ -1,9 +1,27 @@
 // https://app.codility.com/programmers/lessons/4-counting_elements/missing_integer/
 //
-// Task Score: 
-// Correctness: 
-// Performance: 
-// Detected time complexity: 
+// Task Score: 100%
+// Correctness: 100%
+// Performance: 100%
+// Detected time complexity: O(N) or O(N * log(N))
 //
 
+int solution(vector<int> &A) {
+	const int N = 1000001;
+	vector<bool> integers(N, false);
 
+	int n = A.size();
+	for (int i=0; i<n; i++) {
+		if (A[i] > 0) {
+			integers[A[i]] = true;
+		}
+	}
+
+	for (int i=1; i<N; i++) {
+		if (!integers[i]) {
+			return i;
+		}
+	}
+
+	return 0;
+}
