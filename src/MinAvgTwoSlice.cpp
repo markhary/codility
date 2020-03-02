2aa9082c47a13a11479b5b75ebbaa1f1e6f18bdd
Farbod Torabi <20006748+farbod1277@users.noreply.github.com>
2020-03-02 21:43:38 +1100

    Easier to understand solution
    The given solution is unnecessarily complex and convoluted

--- src/MinAvgTwoSlice.cpp
+++ src/MinAvgTwoSlice.cpp
@@ -11,48 +11,35 @@
 
 #include "MinAvgTwoSlice.h"
 
-// Thank you @arthurafarias for the optimized solution
-// The new algorithm reduces complexity from O(N^2) to O(N) by convolving 
-// an average filter of length 3 and length 2. These filters are sufficient 
-// in order to determine the minimum average of any slice from the vector.
+// Easier to understand than previous solution
 int minavgtwoslice::solution(std::vector<int> &A)
 {
-	double minimum = std::numeric_limits<double>::max();
-	int minimum_idx = 0;
-
-	auto avg_filter = [](std::vector<int>::iterator begin, std::vector<int>::iterator end) -> double {
-		double avg = 0;
-		double size = end - begin;
-
-		for (auto it = begin; it < end; it++)
-			avg += (double)*it;
-
-		avg = avg / size;
-
-		return avg;
-	};
-
-	auto minimum_update = [&](double value, int idx) -> void { if (value < minimum) { minimum = value; minimum_idx = idx; } };
-
-	for (unsigned idx = 0; idx < A.size(); idx++)
-	{
-
-		double avg2, avg3;
-
-		if ((idx + 1) < A.size())
-		{
-			avg2 = avg_filter(A.begin() + idx, A.begin() + idx + 2);
-			minimum_update(avg2, (int)idx);
-		}
-
-		if ((idx + 2) < A.size())
-		{
-			avg3 = avg_filter(A.begin() + idx, A.begin() + idx + 3);
-			minimum_update(avg3, (int)idx);
-		}
-	}
-
-	return minimum_idx;
+    unsigned int N = A.size();
+    long int sum;
+    unsigned int minIdx;
+    double avg;
+    double minAvg = std::numeric_limits<double>::max();
+    for(unsigned int i = 0; i < N-1; i++)
+    {
+        sum = A[i] + A[i+1];
+        avg = (double)sum/2;
+        if(avg < minAvg)
+        {
+            minAvg = avg;
+            minIdx = i;
+        }
+        if(i+2 < N)
+        {
+            sum += A[i+2];
+            avg = (double)sum/3;
+            if(avg < minAvg)
+            {
+                minAvg = avg;
+                minIdx = i;
+            }
+        }
+    }
+    return minIdx;
 }
 
 
