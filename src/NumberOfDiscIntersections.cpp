aad445a59c68601505c0b6a39650c1e102cf7ebb
Mark Hary <44713017+markhary@users.noreply.github.com>
2020-03-26 20:02:41 -0700

    Just some editorial comments, code is excellent

--- src/NumberOfDiscIntersections.cpp
+++ src/NumberOfDiscIntersections.cpp
@@ -1,4 +1,4 @@
-// https://app.codility.com/programmers/lessons/5-prefix_sums/min_avg_two_slice/
+// https://app.codility.com/programmers/lessons/6-sorting/number_of_disc_intersections/
 //
 // Task Score: 100%
 // Correctness: 100%
